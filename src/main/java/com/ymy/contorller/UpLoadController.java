bf2fb3eac7e6166c4acaede2d493937d464f4185
ymy <2261279065@qq.com>
2023-09-15 15:55:54 +0800

    文件上传

--- src/main/java/com/ymy/contorller/UpLoadController.java
+++ src/main/java/com/ymy/contorller/UpLoadController.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -19,8 +20,14 @@ public class UpLoadController {
         //获取原始文件名
         String originalFilename = image.getOriginalFilename();
 
+        //构造唯一的文件名（不能重复）--uuid(通用唯一标识)
+        int index = originalFilename.lastIndexOf(".");
+        String extname = originalFilename.substring(index);
+        String newFileName = UUID.randomUUID().toString() + extname;
+        log.info("新的文件名:{}",newFileName);
+
         //将文件存储在服务器的磁盘目录中
-        image.transferTo(new File("D:\\Java\\maven\\StudentManagement\\"+originalFilename));
+        image.transferTo(new File("D:\\Java\\maven\\image\\"+ newFileName));
 
         return Result.success();
     }
