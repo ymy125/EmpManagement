93ec70431ec86c7dcaddd1929e9e4fedd1c39b18
ymy <2261279065@qq.com>
2023-09-15 15:38:42 +0800

    文件上传

--- src/main/java/com/ymy/contorller/UpLoadController.java
+++ src/main/java/com/ymy/contorller/UpLoadController.java
@@ -6,15 +6,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @RestController
 public class UpLoadController {
 
     @PostMapping("/upload")
-    public Result upload(String username, Integer age, MultipartFile image){
+    public Result upload(String username, Integer age, MultipartFile image) throws IOException {
         log.info("上传文件：{},{},{}",username,age,image);
+        //获取原始文件名
+        String originalFilename = image.getOriginalFilename();
 
         //将文件存储在服务器的磁盘目录中
+        image.transferTo(new File("D:\\Java\\maven\\StudentManagement\\"+originalFilename));
 
         return Result.success();
     }
