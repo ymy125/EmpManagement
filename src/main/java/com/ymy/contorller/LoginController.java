b041b814b94fbb36b4ea2ac105425258f1233976
ymy <2261279065@qq.com>
2023-09-17 15:16:04 +0800

    JWT登录

--- src/main/java/com/ymy/contorller/LoginController.java
+++ src/main/java/com/ymy/contorller/LoginController.java
@@ -3,12 +3,19 @@ package com.ymy.contorller;
 import com.ymy.pojo.Emp;
 import com.ymy.pojo.Result;
 import com.ymy.service.EmpService;
+import com.ymy.utils.JwtUtils;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -18,10 +25,22 @@ public class LoginController {
 
     @PostMapping("/login")
     public Result login(@RequestBody Emp emp){
-        log.info("员工登录：{}",emp);
+        log.info("员工登录: {}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+        //登录成功,生成令牌,下发令牌
+        if (e != null){
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
 
+            String jwt = JwtUtils.generateJwt(claims); //jwt包含了当前登录的员工信息
+            return Result.success(jwt);
+        }
+
+//        //登录失败, 返回错误信息
+        return Result.error("用户名或密码错误");
+//        return Result.success();
     }
 
 }
--- src/test/java/com/ymy/StudentManagementApplicationTests.java
+++ src/test/java/com/ymy/StudentManagementApplicationTests.java
@@ -1,13 +1,54 @@
 package com.ymy;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//@SpringBootTest
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGenJwt(){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1);
+        claims.put("name", "tom");
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256,"leon")//设置签名 使用HS256算法，并设置SecretKey(字符串)
+                .setClaims(claims)//自定义内容（载荷）
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))//设置有效期
+                .compact();
+
+        System.out.println(jwt);
+//        JwtBuilder builder= Jwts.builder()
+//                .setId("888")             //设置唯一编号
+//                .setSubject("小白")       //设置主题  可以是JSON数据
+//                .setIssuedAt(new Date())  //设置签发日期
+//                .signWith(SignatureAlgorithm.HS256,"leon");//设置签名 使用HS256算法，并设置SecretKey(字符串)
+//        //构建 并返回一个字符串
+//        System.out.println( builder.compact() );
+
+
+    }
+
+    @Test
+    public void testParseJwt(){
+        Claims claims = Jwts.parser()
+                .setSigningKey("leon")
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTY5NDg4Mzg0MH0.WQHnVbnMC3pyZIoykayCY68J9IoiXnkhRbf3Hw7L3hk")
+                .getBody();
+        System.out.println(claims);
     }
 
 }
